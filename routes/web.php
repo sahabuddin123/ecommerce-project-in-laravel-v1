71a2786e297856095f60baafa756168af01ed1cd
sahab uddin <sahabuddinriyaj984@gmail.com>
2019-09-04 09:50:39 +0600

    create route/admin.php & updated  and modifier route/web.php

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+require 'admin.php';
 Route::get('/', function () {
     return view('welcome');
 });
-Route::view('/admin', 'admin.dashboard.index');
+// Route::view('/admin', 'admin.dashboard.index');
 Route::view('/blankpage', 'admin.dashboard.blank-page');
 Route::view('/charts', 'admin.dashboard.charts');
 Route::view('/fcomponentes', 'admin.dashboard.form-components');
