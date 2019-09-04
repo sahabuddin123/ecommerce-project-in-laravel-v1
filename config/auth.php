1c9990c5fafcedf50bca8f57f30401a61fc50c2e
sahab uddin <sahabuddinriyaj984@gmail.com>
2019-09-04 09:41:33 +0600

    config/auth.php admin provider add

--- config/auth.php
+++ config/auth.php
@@ -81,10 +81,10 @@ return [
             'model' => App\Models\User::class,
         ],
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
+        ],
     ],
 
     /*
